a7e05243378aae2ccf661ef7950eed8fc381c07c
Madalyn Marino <112964220+madalynmm@users.noreply.github.com>
2023-03-10 15:28:22 -0500

    added routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+const { findByPk } = require('../../models/Product');
 
 // The `/api/products` endpoint
 
@@ -7,12 +8,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{model: Category}, {model: Tag}]
+  }).then((productData) => {
+    res.json(productData);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [{ model: Category}, {model: Tag}]
+  }).then((productData) => {
+    res.json(productData);
+  });
 });
 
 // create new product
@@ -91,6 +102,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((deletedProduct) => {
+    res.json(deletedProduct)
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,23 +6,45 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{model: Product}]
+  }).then((tagData) => {
+    res.json(tagData);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [{ model: Product}]
+  }).then((tagData) => {
+    res.json(tagData);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body.tag_name)
+    .then((newTag) => {
+      res.json(newTag);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body.tag_id)
+    .then((updatedTag) => {
+      res.json(updatedTag);
+    }); 
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy(req.params.id)
+    .then((deletedTag) => {
+      res.json(deletedTag);
+    }); 
 });
 
 module.exports = router;
